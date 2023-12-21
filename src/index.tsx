24379daa8146ee134ee70c4b61ed2866b3a0bbbf
Michael H. <michael@iptables.sh>
2023-12-21 14:15:15 +0100

    Instance icon support

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,57 @@
 import { ActionPanel, Detail, List, Action } from "@raycast/api";
 import { execSync } from "child_process";
 
+const ini = require("ini");
+const path = require("path");
 const fs = require("fs");
 const homeDir = require("os").homedir();
-const instancesDir = `${homeDir}/Library/Application Support/PrismLauncher/instances`;
+
+const prismlauncherDir = `${homeDir}/Library/Application Support/PrismLauncher`;
+const prismlauncherConfig = ini.parse(fs.readFileSync(`${prismlauncherDir}/prismlauncher.cfg`, "utf-8"));
+
+const instanceDir = path.isAbsolute(prismlauncherConfig.General.InstanceDir)
+  ? prismlauncherConfig.General.InstanceDir
+  : `${prismlauncherDir}/${prismlauncherConfig.General.InstanceDir}`;
+
+const iconDir = path.isAbsolute(prismlauncherConfig.General.IconsDir)
+  ? prismlauncherConfig.General.IconsDir
+  : `${prismlauncherDir}/${prismlauncherConfig.General.IconsDir}`;
 
 function getInstances() {
   return fs
-    .readdirSync(instancesDir)
-    .filter((file: string) => !file.startsWith(".") && fs.existsSync(`${instancesDir}/${file}/instance.cfg`))
+    .readdirSync(instanceDir)
+    .filter((file: string) => !file.startsWith(".") && fs.existsSync(`${instanceDir}/${file}/instance.cfg`))
     .map((directory: string) => ({ name: directory, key: directory }));
+}
+
+function getInstanceConfig(instance: string) {
+  const config = ini.parse(fs.readFileSync(`${instanceDir}/${instance}/instance.cfg`, "utf-8"));
+
+  // Prism/MMC used to store instance configs without a [General] header, so we need to check for that
+  if (!config.General) {
+    return config;
+  } else {
+    return config.General;
+  }
+}
+
+function getInstanceIcon(instance: string) {
+  const config = getInstanceConfig(instance);
+  // https://github.com/PrismLauncher/PrismLauncher/blob/e39a03421a6e8cdf58f8b5d98388b9eef55d44b7/launcher/icons/IconUtils.cpp#L42
+  const validIconExtensions = [".svg", ".png", ".ico", ".gif", ".jpg", ".jpeg"];
+
+  // We can only display icons that are in the icon directory as Prism doesn't expose the default icons anywhere
+  let icon = "minecraft.svg"
+
+  validIconExtensions.find(extension => {
+    const iconPath = path.join(iconDir, `${config.iconKey}${extension}`);
+    if (fs.existsSync(iconPath)) {
+      console.log(`Found icon for ${instance}: ` + iconPath);
+      icon = iconPath;
+    }
+  });
+
+  return icon;
 }
 
 function launchInstance(instance: string) {
@@ -25,7 +67,7 @@ export default function Command() {
       {instances.map((instance: {key: string, name: string}) => (
         <List.Item
           key={instance.key}
-          icon="minecraft.svg"
+          icon={getInstanceIcon(instance.name)}
           title={instance.name}
           actions={
             <ActionPanel>
